01a4d085ade8374ef2d36838a2776742ebd708ac
Gabe Cloud <gabecloud94@gmail.com>
2022-11-17 13:30:50 -0700

    New Fixed Slider

--- src/components/sections/SimpleSlider.client.jsx
+++ src/components/sections/SimpleSlider.client.jsx
@@ -5,8 +5,7 @@ export function SimpleSlider({products}) {
   const settings = {
     dots: false,
     infinite: true,
-    arrows: false,
-    swipeToSlide: true,
+    arrows: true,
     slidesToShow: 4,
     responsive: [
       {
@@ -28,9 +27,9 @@ export function SimpleSlider({products}) {
       <h1 className="px-6 md:px-8 lg:px-12 py-6 md:py-8 lg:py-12 text-lead font-bold max-w-prose whitespace-pre-wrap">
         Featured Products
       </h1>
-      <Slider {...settings}>
+      <Slider {...settings} className="mr-12">
         {products.map((product) => (
-          <div key={product.id} className="px-4">
+          <div key={product.id} className="pl-4">
             <ProductCard product={product} />
           </div>
         ))}
